e95399962428d2a60e35a7e9f35673a3500aa27a
Ivan Saavedra <ivan.saavedra.iezs@gmail.com>
2023-03-09 13:51:12 -0600

    agregamos funcionalidad a eliminar y completar todos

--- src/App.js
+++ src/App.js
@@ -36,6 +36,22 @@ function App() {
     });
   }
 
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = true;
+
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex,1);
+    setTodos(newTodos);
+  };
+
+
   return (
     <div className="App">
       <div className='App-container'>
@@ -54,6 +70,8 @@ function App() {
               key={todo.text}
               text={todo.text}
               completed={todo.completed}
+              onComplete = {() => completeTodo(todo.text)}
+              onDelete = {() => deleteTodo(todo.text)}
             />
           ))}
 
--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -5,22 +5,23 @@ import { BsFillTrashFill } from 'react-icons/bs';
 
 function TodoItem(props) {
 
-    const onComplete = () => {
-        alert(`Tarea: ${props.text} completada`);
-    }
 
     return(
         // si props.complete != false se concatena 'Icon-check--active'
         <li className="TodoItem">
-            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-                    onClick={onComplete}
+            <span 
+                className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+                onClick={props.onComplete}
             >
             <AiFillCheckCircle />
             </span>
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
                 {props.text} 
             </p>
-            <span className='Icon Icon-delete' ><BsFillTrashFill /></span>
+            <span 
+                className='Icon Icon-delete'
+                onClick={props.onDelete}
+            ><BsFillTrashFill /></span>
         </li>
     )
 }
